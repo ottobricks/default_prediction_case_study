1086cdf7f8feed1ba70f3e78bb5fb1256a341834
Otto von Sperling <otto@vonsperling.com.br>
2021-05-22 20:07:09 -0300

    Move `BinaryUndersampler` to ML module

--- src/5_tuned_model/_aux/ml.py
+++ src/5_tuned_model/_aux/ml.py
@@ -1,9 +1,13 @@
-from typing import Dict, List, Any
+import math
+import multiprocessing as mp
+from typing import Any, Dict, List, Tuple
 import numpy as np
 import pandas as pd
+from scipy import stats
 from sklearn.base import BaseEstimator
 from sklearn.model_selection import GridSearchCV, StratifiedKFold
 
+ZSCORE_CRITICAL_VALUE = 1.645 # equivalent to p-value = 0.05
 
 class EstimatorSelector:
     """
@@ -177,3 +181,54 @@ class EstimatorSelector:
         columns = columns + [c for c in df.columns if c not in columns]
 
         return df[columns]
+
+
+class BinaryUndersampler:
+    
+    def __init__(self, n_iterations: int=5, n_jobs: int=-1):
+        self.n_iterations = n_iterations
+        self.highest_variance_sample = (None, None)
+        self.n_jobs = min(mp.cpu_count(), n_jobs) if n_jobs > 1 else mp.cpu_count()
+    
+    def fit(self, X: np.ndarray, y: np.ndarray) -> Tuple[np.ndarray, np.ndarray, float, float, bool]:
+        """Function to find a subsample of majority class (label) and its significance
+
+        Return
+        ------
+        Tuple[
+            X_subsample, numpy.ndarray: The selected subsample of the majority class
+            y_subsample, numpy.ndarray: The respective labels of the subsample
+            sample_variance, float: The subsample mean variance (mean of variance of each feature)
+            sample_variance_zscore, float: The bootstrap Z-score of the `sample_variance` from `n_iterations`,
+            is_sample_var_significantly_higher, bool: Whether the `sample_variance` is significantly higher than the bootstrap mean (p-value < 0.05)
+        ]
+        """
+        self.majority_class = pd.value_counts(pd.Series(y)).index[0]
+        self.n_splits = math.floor(X.shape[0] / np.sum(y!=self.majority_class))
+        # split and select the sample with highest variance (decide how significant via bootstraping)
+        with mp.Pool(self.n_jobs) as pool:
+            bootstrap_samples = pool.starmap(
+                self.get_highest_variance_sample,
+                [(X[y==self.majority_class], y[y==self.majority_class], self.n_splits, idx) for idx in range(self.n_iterations)]
+            )
+        highest_zscore = sorted(stats.zscore(np.array(list(map(lambda tup: tup[-1], bootstrap_samples)))))[::-1][0]
+        is_sample_var_significantly_higher = highest_zscore > ZSCORE_CRITICAL_VALUE
+        highest_mean_var_sample = sorted(bootstrap_samples, key=lambda tup: tup[-1])[::-1][0]
+        return (*highest_mean_var_sample, highest_zscore, is_sample_var_significantly_higher)
+        
+
+    def get_highest_variance_sample(self, X: np.ndarray, y_label: np.ndarray, n_splits: int, random_state: int=42):
+        disjoint_samples = self._shuffle_split_observations(X, y_label, n_splits, random_state)
+        disjoint_samples_mean_var = map(self._get_mean_feature_variance, disjoint_samples)
+        highest_mean_var_sample = sorted(disjoint_samples_mean_var, key=lambda tup: tup[-1])[::-1][0]
+        return highest_mean_var_sample
+
+    def _shuffle_split_observations(self, X: np.ndarray, y_label: np.ndarray, n_splits: int, random_state: int=42) -> List:
+        """Return a list of disjoint samples with the `y_label` concatenated at axis=1 (column)"""
+        np.random.seed(random_state)
+        return np.array_split(np.random.permutation(np.concatenate((X, y_label.reshape(-1, 1)), axis=1)), n_splits)
+
+    def _get_mean_feature_variance(self, sample: np.ndarray) -> Tuple[np.ndarray, np.ndarray, float]:
+        X, y_label = sample[:, :-1], sample[:, -1]
+        mean_variance = np.mean(np.ma.var(X, axis=0))
+        return (X, y_label, mean_variance)
