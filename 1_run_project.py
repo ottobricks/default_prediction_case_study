c76fe35c17a94985b435672c9323fcbc39a12fd6
Otto von Sperling <otto@vonsperling.com.br>
2021-05-23 21:45:07 -0300

    Update script to run project

--- 1_run_project.py
+++ 1_run_project.py
@@ -6,6 +6,8 @@ subprocess.run("poetry run jupytext --to py:percent src/0_split_data/split_train
 subprocess.run("poetry run jupytext --to py:percent src/3_feature_engineering/1_define_preprocessors.ipynb".split())
 subprocess.run("poetry run jupytext --to py:percent src/3_feature_engineering/2_transform_data.ipynb".split())
 subprocess.run("poetry run jupytext --to py:percent src/4_baseline_model/1_train_and_score.ipynb".split())
+subprocess.run("poetry run jupytext --to py:percent src/5_tuned_model/*.ipynb".split())
+subprocess.run("poetry run jupytext --to py:percent src/6_results/*.ipynb".split())
 print("\n\tDone preparing!\n")
 
 print("Running script to hold out test set...")
@@ -23,3 +25,15 @@ print("\n\tDone with data transformation script!\n")
 print("Running script to fit baseline and score validation...")
 subprocess.run("poetry run python ./1_train_and_score.py".split(), cwd=f"{os.getcwd()}/src/4_baseline_model/")
 print("\n\tDone with baseline script!\n")
+
+print("Running script to fit handle imbalanced_data...")
+subprocess.run("poetry run python ./1_handle_imbalanced.py".split(), cwd=f"{os.getcwd()}/src/5_tuned_model/")
+print("\n\tDone with imbalance script!\n")
+
+print("Running script to select models...")
+subprocess.run("poetry run python ./2_select_hyperparams_and_models.py".split(), cwd=f"{os.getcwd()}/src/5_tuned_model/")
+print("\n\tDone with model selection script!\n")
+
+print("Running script to fit best model and score data...")
+subprocess.run("poetry run python ./1_train_and_score.py".split(), cwd=f"{os.getcwd()}/src/6_results/")
+print("\n\tDone with scoring script!\n")
